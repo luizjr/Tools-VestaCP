8b34baa5fa97de2009204dbbe519c954bab709e5
Luiz Silva <luizjrdeveloper@gmail.com>
2018-03-08 14:15:38 -0300

    Teste

--- install.sh
+++ install.sh
@@ -14,7 +14,13 @@ read opcao
 
 if [ "$opcao" -eq 1 ]
 then
-	echo 'Vamos começar a ativação ' && mkdir /usr/local/scripts/ && cd /usr/local/scripts/ && git clone https://github.com/luizjrdeveloper/filemanager-vestacp.git && cd filemanager-vestacp && echo "FILEMANAGER_KEY=''" >> /usr/local/vesta/conf/vesta.conf && cp filemanager.sh /usr/local/scripts/ && chmod a+x /usr/local/scripts/filemanager.sh && chown admin:admin /usr/local/scripts/filemanager.sh && echo "admin   ALL=NOPASSWD:/usr/local/scripts/*" >> /etc/sudoers.d/admin && cd .. && rm -rf filemanager-vestacp
+	if [ ! -d "/usr/local/scripts/" ];
+	then
+		&& cd /usr/local/scripts/
+	elif
+		&&  mkdir /usr/local/scripts/ && cd /usr/local/scripts/
+	fi
+	echo 'Vamos começar a ativação ' && git clone https://github.com/luizjrdeveloper/filemanager-vestacp.git && cd filemanager-vestacp && echo "FILEMANAGER_KEY=''" >> /usr/local/vesta/conf/vesta.conf && cp filemanager.sh /usr/local/scripts/ && chmod a+x /usr/local/scripts/filemanager.sh && chown admin:admin /usr/local/scripts/filemanager.sh && echo "admin   ALL=NOPASSWD:/usr/local/scripts/*" >> /etc/sudoers.d/admin && cd .. && rm -rf filemanager-vestacp
 
 elif [ "$opcao" -eq 2 ]
 then
