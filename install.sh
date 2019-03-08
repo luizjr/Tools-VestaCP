a252a2184d0898a151227144e57e0312d16d765c
Luiz Silva <luizjrdeveloper@gmail.com>
2019-03-08 17:30:50 -0300

    Atualizando Script para instalação do SSL

--- install.sh
+++ install.sh
@@ -3,139 +3,199 @@
 #author Luiz Jr
 #created 10/03/2018
 
-clear
+### Menu da aplicação
+menu(){
+	### Limpando o Terminal
+	clear
+	### Escrevendo menu na tela
+	echo '=========================================='
+	echo 'Bem Vindo ao Canivete Suíço do VestaCP'
+	echo '=========================================='
+	echo ' 1 -> Atualizar php7.0 para php7.1'
+	echo ' 2 -> Instalar SSL para VestaCP Painel e E-mail'
+	echo ' 3 -> Ativar FileManager'
+	echo ' 4 -> Instalar Templates Laravel, ReactJS e HTTPS'
+	echo ' 5 -> Todas as Opções'
+	echo ' 6 -> Sair'
+	echo '============================== By Luiz Jr'
+	### Aguardando Input do Usuário
+	### Aguardando a opção ser selecionada
+	read -p 'Escolha a opção e pressione [ENTER]: ' opcao
+}
 
-echo 'Bem Vindo ao Canivete Suíço do VestaCP'
-echo '======'
-echo ' 1 -> Atualizar php7.0 para php7.1'
-echo ' 2 -> Instalar SSL para VestaCP Painel e E-mail'
-echo ' 3 -> Ativar FileManager'
-echo ' 4 -> Instalar Templates Laravel, ReactJS e HTTPS'
-echo ' 5 -> Sair'
+### Atualização do php7.0 para o php7.1
+atualizacao_php71(){
+	echo "Vamos começar a atualização do php7.0 para o php7.1"
+	# Atualizando Pacotes e preparando sistema
+	sudo apt update
+	sudo apt install software-properties-common python-software-properties -y
+	sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php -y
+	sudo apt update
+	sudo apt install php7.1 -y
+	a2dismod php7.0
+	a2enmod php7.1
+	# Instalando Módulos
+	sudo apt install php7.1-common php7.1-zip libapache2-mod-php7.1 php7.1-cgi php7.1-cli php7.1-phpdbg php7.1-fpm libphp7.1-embed php7.1-dev php7.1-curl php7.1-gd php7.1-imap php7.1-interbase php7.1-intl php7.1-ldap php7.1-mcrypt php7.1-readline php7.1-odbc php7.1-pgsql php7.1-pspell php7.1-recode php7.1-tidy php7.1-xmlrpc php7.1 php7.1-json php-all-dev php7.1-sybase php7.1-sqlite3 php7.1-mysql php7.1-opcache php7.1-bz2 libapache2-mod-php7.1 php7.1-mbstring php7.1-pdo php7.1-dom -y
+	# Reiniciando Apache
+	sudo service apache2 restart
+}
 
-read -p 'Escolha a opção e pressione [ENTER]: ' opcao
+### Instala Certificado SSL para o Painel VestaCP
+instala_ssl_no_painel(){
+	echo "Iniciando a Instalação do SSL..."
+	sleep 1
+	echo "Emitindo Certificado SSL para o Painel VestaCP..."
+	/usr/local/vesta/bin/v-add-letsencrypt-domain 'admin' $HOSTNAME '' 'yes'
 
-if [[ -n "$opcao" ]]; then
-	if [ "$opcao" -eq 1 ]; then
-		echo "Vamos começar a atualização do php7.0 para o php7.1"
-		sudo apt update
-		sudo apt install software-properties-common python-software-properties -y
-		sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php -y
-		apt update
-		apt install php7.1 -y
-		a2dismod php7.0
-		a2enmod php7.1
+	echo "Isso aplicará o SSL instalado aos daemons VestaCP, Exim e Dovecot."
+	/usr/local/vesta/bin/v-update-host-certificate admin $HOSTNAME
 
-		#Instalando Módulos
-		apt install php7.1-common php7.1-zip libapache2-mod-php7.1 php7.1-cgi php7.1-cli php7.1-phpdbg php7.1-fpm libphp7.1-embed php7.1-dev php7.1-curl php7.1-gd php7.1-imap php7.1-interbase php7.1-intl php7.1-ldap php7.1-mcrypt php7.1-readline php7.1-odbc php7.1-pgsql php7.1-pspell php7.1-recode php7.1-tidy php7.1-xmlrpc php7.1 php7.1-json php-all-dev php7.1-sybase php7.1-sqlite3 php7.1-mysql php7.1-opcache php7.1-bz2 libapache2-mod-php7.1 php7.1-mbstring php7.1-pdo php7.1-dom -y
-		service apache2 restart
-		exit 1
+	echo "Isso permitirá que o VestaCP atualize o SSL para os daemons VestaCP, Exim e dovecot toda vez que o SSL for renovado."
+	echo "UPDATE_HOSTNAME_SSL='yes'" >> /usr/local/vesta/conf/vesta.conf
 
-	elif [ "$opcao" -eq 2 ]; then
-		echo "Iniciando a Instalação do SSL..."
-		sleep 1
-		echo "Emitindo Certificado SSL para o Painel VestaCP..."
-		/usr/local/vesta/bin/v-add-letsencrypt-domain 'admin' $HOSTNAME '' 'yes'
+	sleep 2
+	echo "SSL Ativado para o Painel com Sucesso!"
+}
 
-		echo "Isso aplicará o SSL instalado aos daemons VestaCP, Exim e Dovecot."
-		/usr/local/vesta/bin/v-update-host-certificate admin $HOSTNAME
+### Ativação do FileManager
+ativar_gerenciador_de_arquivos(){
+	echo "Iniciando Ativação do FileManager..."
+	sleep 1
 
-		echo "Isso permitirá que o VestaCP atualize o SSL para os daemons VestaCP, Exim e dovecot toda vez que o SSL for renovado."
-		echo "UPDATE_HOSTNAME_SSL='yes'" >> /usr/local/vesta/conf/vesta.conf
+	### Variáveis
+	file_vesta_filemanager="/etc/cron.hourly/vesta_filemanager"
+	file_emp="FILEMANAGER_KEY=''"
+	file_tex="FILEMANAGER_KEY='ILOVEREO'"
 
+	ativa_file_manager(){
+		echo '
+		#! /bin/bash
+		#created luizjrdeveloper@gmail.com
+		#author Luiz Jr
+		#created 10/03/2018
+
+		a="'${file_emp}'"
+		b="'${file_tex}'"
+
+		if grep -Fxq "$b" /usr/local/vesta/conf/vesta.conf
+		then
+			sed -i -e "s/$b/$b/g" /usr/local/vesta/conf/vesta.conf
+		else
+			if grep -Fxq "$a" /usr/local/vesta/conf/vesta.conf
+			then
+				# Encontrou a TAG
+				sed -i -e "s/$a/$b/g" /usr/local/vesta/conf/vesta.conf
+			else
+				echo $b >> /usr/local/vesta/conf/vesta.conf
+			fi
+		fi' >> $file_vesta_filemanager
+		chmod +x $file_vesta_filemanager
+		echo "Ativando FileManager..."
+		echo 'admin  ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+		#Adicionando tarefa para o admin ativar o FileManager
+		/usr/local/vesta/bin/v-add-cron-job admin "*/2" "*" "*" "*" "*" "sudo /bin/bash /etc/cron.hourly/vesta_filemanager"
+		bash $file_vesta_filemanager
 		sleep 2
-		echo "SSL Ativado para o Painel com Sucesso!"
-		exit 1
+		echo "FileManager Ativado com Sucesso!"
+	}
+
+	### Verifica se é uma reinistalação ou uma nova instalçao
+	if [ -f "$file_vesta_filemanager" ]; then
+		echo "Limpando Ativação Anterior..."
+		rm $file_vesta_filemanager
+		ativa_file_manager
+	else
+		ativa_file_manager
+	fi
+}
+
+instala_templates_vestacp(){
+	echo "Instalando Templates..."
+	patch_template="/usr/local/vesta/data/templates/web"
+	git clone https://github.com/luizjrdeveloper/tools-vestacp.git
+	cp -R tools-vestacp/includes/apache2 $patch_template
+	cp -R tools-vestacp/includes/nginx $patch_template
+	rm -R tools-vestacp
+	echo "Templates Instados!"
+}
+
+opcoes(){
+	### Verifica a opção selecionada no menu
+	if [[ -n "$opcao" ]]; then
+
+		# Atualiza o PHP
+		if [ "$opcao" -eq 1 ]; then
+			atualizacao_php71
+			sair_ou_continuar
+
+		# Instal SSL no Painel VestaCP
+		elif [ "$opcao" -eq 2 ]; then
+			instala_ssl_no_painel
+			sair_ou_continuar
 
 		# Ativacao do FileManager
-	elif [ "$opcao" -eq 3 ]; then
-		echo "Iniciando Ativação do FileManager..."
-		sleep 1
-		file_vesta_filemanager="/etc/cron.hourly/vesta_filemanager"
-		file_emp="FILEMANAGER_KEY=''"
-		file_tex="FILEMANAGER_KEY='ILOVEREO'"
+		elif [ "$opcao" -eq 3 ]; then
+			ativar_gerenciador_de_arquivos
+			sair_ou_continuar
 
-		if [ -f "$file_vesta_filemanager" ]; then
-			echo "Limpando Ativação Anterior..."
-			rm $file_vesta_filemanager
+		# Instalando Templates para o VestaCP
+		elif [ "$opcao" -eq 4 ]; then
+			instala_templates_vestacp
+			sair_ou_continuar
 
-			echo '
-			#! /bin/bash
-			#created luizjrdeveloper@gmail.com
-			#author Luiz Jr
-			#created 10/03/2018
+		# Todas as opções
+		elif [ "$opcao" -eq 5 ]; then
+			atualizacao_php71
+			instala_ssl_no_painel
+			ativar_gerenciador_de_arquivos
+			instala_templates_vestacp
+			sair_ou_continuar
 
-			a="'${file_emp}'"
-			b="'${file_tex}'"
-
-			if grep -Fxq "$b" /usr/local/vesta/conf/vesta.conf
-			then
-				sed -i -e "s/$b/$b/g" /usr/local/vesta/conf/vesta.conf
-			else
-				if grep -Fxq "$a" /usr/local/vesta/conf/vesta.conf
-				then
-					# Encontrou a TAG
-					sed -i -e "s/$a/$b/g" /usr/local/vesta/conf/vesta.conf
-				else
-					echo $b >> /usr/local/vesta/conf/vesta.conf
-				fi
-			fi' >> $file_vesta_filemanager
-			chmod +x $file_vesta_filemanager
-			echo "Ativando FileManager..."
-			bash $file_vesta_filemanager
-			sleep 2
-			echo "FileManager Ativado com Sucesso!"
+		# Fecha a aplicação
+		elif [ "$opcao" -eq 6 ]; then
+			sair
 		else
-			echo '
-			#! /bin/bash
-			#created luizjrdeveloper@gmail.com
-			#author Luiz Jr
-			#created 10/03/2018
-
-			a="'${file_emp}'"
-			b="'${file_tex}'"
-
-			if grep -Fxq "$b" /usr/local/vesta/conf/vesta.conf
-			then
-				sed -i -e "s/$b/$b/g" /usr/local/vesta/conf/vesta.conf
-			else
-				if grep -Fxq "$a" /usr/local/vesta/conf/vesta.conf
-				then
-					# Encontrou a TAG
-					sed -i -e "s/$a/$b/g" /usr/local/vesta/conf/vesta.conf
-				else
-					echo $b >> /usr/local/vesta/conf/vesta.conf
-				fi
-			fi' >> $file_vesta_filemanager
-			chmod +x $file_vesta_filemanager
-			echo "Ativando FileManager..."
-			echo 'admin  ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
-			#Adicionando tarefa para o admin ativar o FileManager
-			/usr/local/vesta/bin/v-add-cron-job admin "*/2" "*" "*" "*" "*" "sudo /bin/bash /etc/cron.hourly/vesta_filemanager"
-			bash $file_vesta_filemanager
-			sleep 2
-			echo "FileManager Ativado com Sucesso!"
+			clear && echo "Opção inválida" && sleep 0.5 && clear
+			aplicacao
 		fi
-		exit 1
-	elif [ "$opcao" -eq 4 ]; then
-		echo "Instalando Templates..."
-		patch_template="/usr/local/vesta/data/templates/web"
-		git clone https://github.com/luizjrdeveloper/tools-vestacp.git
-		cp -R tools-vestacp/includes/apache2 $patch_template
-		cp -R tools-vestacp/includes/nginx $patch_template
-		rm -R tools-vestacp
-		echo "Templates Instados!"
-		sleep 2
-		exit 1
-	elif [ "$opcao" -eq 5 ]; then
-		echo "Cancelando a ativação..."
-		sleep 3
-		exit 1
 	else
-		echo "Opção inválida"
-		exit 0
+		clear && echo "Opção inválida" && sleep 0.5 && clear
+		aplicacao
 	fi
-else
-	echo "Opção inválida"
-	exit 0
-fi
+}
+
+sair_ou_continuar(){
+	### Limpando o Terminal
+	clear
+	### Escrevendo menu na tela
+	echo 'Deseja realmente sair?'
+	echo ' 1 -> Sim'
+	echo ' 2 -> Não'
+
+	### Aguardando a opção ser selecionada
+	read -p 'Escolha a opção e pressione [ENTER]: ' opcao_para_fechar
+
+	if [[ -n "$opcao_para_fechar" ]]; then
+		if [ "$opcao_para_fechar" -eq 2 ]; then
+			aplicacao
+		elif [ "$opcao_para_fechar" -eq 1 ]; then
+			# Fecha aplicação
+			sair
+		else
+			sair_ou_continuar
+		fi
+	fi
+}
+
+sair(){
+	### Limpando o Terminal
+	clear && echo "Fechando a aplicação..." && sleep 1 && clear
+	exit
+}
+
+aplicacao(){
+	menu
+	opcoes
+}
+
+aplicacao
